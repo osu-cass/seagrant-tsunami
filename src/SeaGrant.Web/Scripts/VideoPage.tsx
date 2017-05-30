f04a850605c3b8af7cd977469d02e3cca655f7af
Garrett Smith <garrett.smith@oregonstate.edu>
2017-05-30 15:16:29 -0700

    Added video page template

---
+++ src/SeaGrant.Web/Scripts/VideoPage.tsx
@@ -0,0 +1,30 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+namespace VideoPage {
+
+    interface VideoProps {
+        source: string;
+        name: string;
+    }
+
+    export interface props {
+
+    }
+
+    export interface state {
+
+    }
+
+
+    function initVideoPage() {
+        const mainDiv = document.getElementById("main") as HTMLElement;
+        const menuDiv = document.getElementById("menu") as HTMLElement;
+
+       // ReactDOM.render();
+    }
+
+}
+
+
