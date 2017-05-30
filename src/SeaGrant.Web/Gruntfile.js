567fd078d621c253aadd6e4dad8b0d6f30a28904
Garrett Smith <garrett.smith@oregonstate.edu>
2017-05-30 14:09:57 -0700

    Project base

---
+++ src/SeaGrant.Web/Gruntfile.js
@@ -0,0 +1,86 @@
+﻿/// <binding ProjectOpened='all, watch' />
+'use strict';
+
+const lessFiles = {
+    'wwwroot/css/site.css': 'Styles/site.less',
+};
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: {
+            css: ["wwwroot/css/*"],
+            ts: ["wwwroot/scripts/*", "temp"],
+            lib: ["wwwroot/lib/*", "temp"]
+        },
+
+        uglify: {
+            files: {
+                src: 'wwwroot/scripts/*.js',  // source files mask
+                dest: 'wwwroot/scripts/',    // destination folder
+                expand: true,    // allow dynamic building
+                flatten: true,   // remove all unnecessary nesting
+                ext: '.min.js'   // replace .js to .min.js
+            }
+        },
+
+        less: {
+            development: {
+                files: lessFiles
+            },
+            production: {
+                files: lessFiles
+            }
+        },
+
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    "wwwroot/css/site.min.css": ["wwwroot/css/site.css"]
+                }
+            }
+        },
+
+        ts: {
+            default: {
+                tsconfig: {
+                    tsconfig: 'Scripts/tsconfig.json',
+                },
+            },
+        },
+
+        watch: {
+            less: {
+                files: ["Styles/*.less"],
+                tasks: ["less"]
+            }
+        },
+        "bower-install-simple": {
+            options: {
+            },
+            "prod": {
+                options: {
+                }
+            }
+
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-bower-install-simple');
+
+
+    grunt.registerTask("bower-install", ["bower-install-simple"]);
+    grunt.registerTask("all", ['clean', 'ts', 'less', 'cssmin', 'uglify', 'bower-install']);
+    grunt.registerTask("tsrecompile", ['clean:ts', 'ts', 'uglify']);
+    grunt.registerTask("lessrecompile", ['clean:css', 'less', 'cssmin']);
+
+};
---
+++ src/SeaGrant.Web/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace SeaGrant.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseStaticFiles();
+
+
+            if (env.IsDevelopment())
+            {
+                var options = new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Scripts")),
+                    RequestPath = new Microsoft.AspNetCore.Http.PathString("/Scripts"),
+                    ServeUnknownFileTypes = true
+                };
+                app.UseStaticFiles(options);
+
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
