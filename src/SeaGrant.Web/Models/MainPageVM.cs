023b11bc7efe0b812a170f79a0784c9a384731b4
Garrett Smith <garrett.smith@oregonstate.edu>
2017-06-08 14:54:38 -0700

    Option to reset page after inactivity

--- src/SeaGrant.Web/Models/MainPageVM.cs
+++ src/SeaGrant.Web/Models/MainPageVM.cs
@@ -9,9 +9,11 @@ namespace SeaGrant.Web.Models
     public class MainPageVM
     {
         public ImmutableArray<Place> Places { get; }
-        public MainPageVM(ImmutableArray<Place> places)
+        public bool DemoMode { get; }
+        public MainPageVM(ImmutableArray<Place> places, bool demoMode)
         {
             Places = places;
+            DemoMode = demoMode;
         }
     }
 }
